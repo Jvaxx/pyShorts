1f10e0197b65c4b28b6fbbee1f4dc3e5521f524d
Jvaxx <jeanvaleryze@gmail.com>
2024-06-05 14:46:28 +0200

    fix import bug

--- TextGen/__init__.py
+++ TextGen/__init__.py
@@ -1,2 +1,2 @@
-from .core import TextGeneration, TextToSpeechEleven, VideoGenerator, TextGenerationOllama, BatchTextGenerator, BatchVideoGeneratorFromFile
+from .core import TextToSpeechEleven, VideoGenerator, TextGenerationOllama, BatchTextGenerator, BatchVideoGeneratorFromFile
 from .helpers import *
